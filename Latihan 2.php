451ce81a508aeee96ce4b203272892a19727e641
rails s <34225598+fajaruto14@users.noreply.github.com>
2019-10-08 21:57:43 +0700

    Latihan 2 (PHP)

---
+++ Latihan 2.php
@@ -0,0 +1,36 @@
+<?php 
+// KALKULATOR LATIHAN 2
+$a = 15;
+$b = 5;
+
+echo "Bilangan 1 = $a"; echo "<br>";
+echo "Bilangan 2 = $b"; echo "<br>";
+echo "Berikut hasil perhitungan dari setiap operator"; echo "<br>";
+
+	echo "<br/> PENJUMLAHAN <br/>";
+	$c = $a+$b;
+	echo "Operator = +"; echo "<br>";
+	echo "Hasilnya : $a + $b = $c";
+	echo "<br/>";
+	echo "<br/> PENGURANGAN <br/>";
+	$d = $a-$b;
+	echo "Operator = -"; echo "<br>";
+	echo "Hasilnya : $a - $b = $d";
+	echo "<br/>";
+	echo "<br/> PERKALIAN <br/>";
+	$e = $a*$b;
+	echo "Operator = *"; echo "<br>";
+	echo "Hasilnya : $a * $b = $e";
+	echo "<br/>";
+	echo "<br/> PEMBAGIAN <br/>";
+	$f = $a+$b;
+	echo "Operator = /"; echo "<br>";
+	echo "Hasilnya : $a / $b = $f";
+	echo "<br/>";
+	echo "<br/> PENJUMLAHAN <br/>";
+	$g = $a%$b;
+	echo "Operator = %"; echo "<br>";
+	echo "Hasilnya : $a % $b = $g";
+	echo "<br/>";
+
+ ?>
